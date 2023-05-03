0906426513422051f08eb2cdca1aef3646ceeb8c
nicolasblancom <nicolasblancom@gmail.com>
2023-05-03 13:19:06 +0200

    Initial commit

---
+++ src/edit.js
@@ -0,0 +1,53 @@
+/**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * React hook that is used to mark the block wrapper element.
+ * It provides all the necessary props like the class name.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
+ */
+import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
+
+import { Panel, PanelBody, TextControl } from '@wordpress/components';
+
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * Those files can contain any CSS code that gets applied to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+import './editor.scss';
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
+ *
+ * @return {WPElement} Element to render.
+ */
+export default function Edit( {attributes, setAttributes} ) {
+    return (
+        <div {...useBlockProps()}>
+            <InspectorControls key="setting">
+				<Panel>
+					<PanelBody title="Subscribe attributes" initialOpen={true}>
+						<TextControl className="blocks-base-control__input"
+									label={"Heading"}
+									value={attributes.heading}
+									onChange={(val) => setAttributes({heading: val})}
+						/>
+					</PanelBody>
+				</Panel>
+			</InspectorControls>
+
+			<h2 className={"subscribe-header"}>{attributes.heading}</h2>
+            <p>Visit our <a href="/subscribe">subscribe page</a> and sign up to get notified</p>
+        </div>
+    );
+}
---
+++ wp-learn-subscribe.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Plugin Name: WP Learn Subscribe
+ * Description: Using a shortcode, render a subscribe area anywhere on your site
+ * Version: 1.0.0
+ * Text Domain: wp-subscribe
+ * License: GPLv2 or later
+ *
+ * @package WP_Learn_Subscribe
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+add_shortcode( 'wp-subscribe', 'wps_subscribe_shortcode' );
+function wps_subscribe_shortcode() {
+	ob_start();
+	?>
+	<div class="wp-subscribe-subscribe">
+		<h2>Subscribe!</h2>
+		<p>Visit our <a href="/subscribe">subscribe page</a> and sign up to get notified</p>
+	</div>
+	<?php
+	return ob_get_clean();
+}
+
+/**
+* Registers the block using the metadata loaded from the `block.json` file.
+* Behind the scenes, it registers also all assets so they can be enqueued
+* through the block editor in the corresponding context.
+*
+* @see https://developer.wordpress.org/reference/functions/register_block_type/
+*/
+add_action( 'init', 'wpl_subscribe_block_init' );
+function wpl_subscribe_block_init() {
+    register_block_type( __DIR__ . '/build' );
+}
